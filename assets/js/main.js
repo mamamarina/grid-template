c8d31cd2af6bc4468af9b10c768683a0b25999c9
Maryna Tuboleva <mstuboleva@gmail.com>
2020-11-18 20:44:21 +0200

    errors correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	$('.header-navbar__btn').on('click', function() {
-		$(this).toggleClass('header-navbar__btn--active');
-		$('.header-navbar__list').toggleClass('header-navbar__list--active');
+	  $('.header-navbar__btn').on('click', function(){ 
+	  $(this).toggleClass('header-navbar__btn--active');
+	  $('.header-navbar__list').toggleClass('header-navbar__list--active');
 	});
-});
+  });
 
 
-// $(document).ready(function(){
-// 	$('.header-navbar__btn').on('click', function(){
-// 	  $(this).toggleClass('header-navbar__btn--active');
-// 	  $('.header-navbar__list').stop(true, true).slideToggle(500);
-// 	});
-//   });
+
+  
+
+
+
+
