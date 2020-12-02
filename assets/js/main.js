9075be4d1b34781e495c49210a6cced02d1636df
Maryna Tuboleva <mstuboleva@gmail.com>
2020-12-02 22:59:59 +0200

    исправлены ошибки в секциях skills,form, navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-	  $('.header-navbar__btn').on('click', function(){ 
-	  $(this).toggleClass('header-navbar__btn--active');
-	  $('.header-navbar__list').toggleClass('header-navbar__list--active');
-	});
+// $(document).ready(function() {
+// 	  $('.header-navbar__btn').on('click', function(){ 
+// 	  $(this).toggleClass('header-navbar__btn--active');
+// 	  $('.header-navbar__list').toggleClass('header-navbar__list--active');
+// 	});
+//   });
+
+$(document).ready(function(){
+  $('.header-navbar__btn').click(function(){
+    $(this).toggleClass('active');
+    $('.header-navbar__list').slideToggle(500);
   });
+});
 
 
 
